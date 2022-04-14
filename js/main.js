bece6fad17c97a75732f06b58e0a696a5ed7e7ce
Pedro Almeida <pedroalmeida.programador@gmail.com>
2022-04-14 16:01:36 -0300

    Contato Finalizado

--- js/main.js
+++ js/main.js
@@ -33,3 +33,46 @@ modalClose.forEach((mc) => {
         })
     })
 })
+
+/*========= MIXITUP FILTER PORTFOLIO =========*/
+
+var mixerPortfolio = mixitup('.work__container', {
+    selectors: {
+        target: '.work__card'
+    },
+    animation: {
+        duration: 300
+    }
+});
+
+/* Link active work */
+const linkWork = document.querySelectorAll('.work__item');
+
+function activeWork() {
+    linkWork.forEach(l => l.classList.remove('active-work'))
+    this.classList.add('active-work')
+}
+
+linkWork.forEach(l => l.addEventListener('click', activeWork))
+
+/*========= SWIPER TESTIMONIAL =========*/
+
+var swiperTestimonial = new Swiper(".testimonial__container", {
+    spaceBetween: 27,
+    loop: true,
+    grabCursor: true,
+
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        576: {
+          slidesPerView: 2,
+        },
+        768: {
+          slidesPerView: 4,
+          spaceBetween: 48,
+        },
+      },
+  });
